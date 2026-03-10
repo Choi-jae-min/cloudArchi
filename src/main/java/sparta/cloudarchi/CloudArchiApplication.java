175851b8614d1c53e8bd49127a2cd355f1c994c6
jasmin <cjm98@smartel.co.kr>
2026-03-10 11:31:23 +0900

    Initial commit

---
+++ src/main/java/sparta/cloudarchi/CloudArchiApplication.java
@@ -0,0 +1,13 @@
+package sparta.cloudarchi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudArchiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudArchiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sparta/cloudarchi/CloudArchiApplicationTests.java
@@ -0,0 +1,13 @@
+package sparta.cloudarchi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudArchiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
