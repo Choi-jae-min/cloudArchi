bfd0ac59f92b279fba6de62a13628d0c8ef1c9d3
jasmin <cjm98@smartel.co.kr>
2026-03-11 13:46:15 +0900

    build : spring 4 -> 3 변경

--- src/test/java/sparta/cloudarchi/CloudArchiApplicationTests.java
+++ src/test/java/sparta/cloudarchi/CloudArchiApplicationTests.java
@@ -2,9 +2,14 @@ package sparta.cloudarchi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import sparta.cloudarchi.global.s3.S3Service;
 
 @SpringBootTest
 class CloudArchiApplicationTests {
+
+    @MockBean
+    private S3Service s3Service;
 
     @Test
     void contextLoads() {
